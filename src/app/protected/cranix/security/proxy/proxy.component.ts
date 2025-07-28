5609d965cb3624a0d042c27f3f1c8fecd7c5c14a
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2025-07-28 20:31:22 +0200

    Remove ag-grid from proxy.component

--- src/app/protected/cranix/security/proxy/proxy.component.ts
+++ src/app/protected/cranix/security/proxy/proxy.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { GridApi } from 'ag-grid-community'
+import { Component } from '@angular/core';
 
 import { LanguageService } from 'src/app/services/language.service';
 import { AuthenticationService } from 'src/app/services/auth.service';
@@ -7,14 +6,15 @@ import { SecurityService } from 'src/app/services/security-service';
 import { CheckBoxBTNRenderer } from 'src/app/pipes/ag-checkbox-renderer';
 import { GenericObjectService } from 'src/app/services/generic-object.service';
 import { ApplyCheckBoxBTNRenderer } from 'src/app/pipes/ag-apply-checkbox-renderer';
+import { aC } from '@fullcalendar/core/internal-common';
 
 @Component({
   standalone: false,
-    selector: 'cranix-proxy',
+  selector: 'cranix-proxy',
   templateUrl: './proxy.component.html',
   styleUrls: ['./proxy.component.scss'],
 })
-export class ProxyComponent implements OnInit {
+export class ProxyComponent {
 
   segment = 'basic';
   newDomain: string = '';
@@ -22,12 +22,11 @@ export class ProxyComponent implements OnInit {
   blackList: string[] = [];
   whiteList: string[] = [];
   cephalixList: string[] = [];
-  lists  = {};
+  groups: string[] = []
+  lists = {};
   proxyOptions;
   context;
-  proxyApi: GridApi;
   proxySelected;
-  columnDefs: any[];
 
   constructor(
     public authService: AuthenticationService,
@@ -36,58 +35,27 @@ export class ProxyComponent implements OnInit {
     public securityService: SecurityService
   ) {
     this.context = { componentParent: this };
-  }
-
-  ngOnInit() {
-    this.readLists('good').then( val => { this.lists['good'] = val.sort() } );
-    this.readLists('bad').then( val =>  { this.lists['bad'] = val.sort() } );
+    this.readLists('good').then(val => { this.lists['good'] = val.sort() });
+    this.readLists('bad').then(val => { this.lists['bad'] = val.sort() });
     if (this.authService.session.name == 'cephalix' || this.authService.isAllowed('cephalix.manage')) {
-      this.readLists('cephalix').then( val => {this.lists['cephalix'] = val.sort() } );
+      this.readLists('cephalix').then(val => { this.lists['cephalix'] = val.sort() });
     }
     console.log(this.lists);
     this.readDatas().then(val => {
+      this.groups = Object.getOwnPropertyNames(val[0]).filter(value => value != 'name')
       this.rowData = val;
-      this.columnDefs = [{
-        field: 'name',
-        headerName: this.languageS.trans('name'),
-        suppressHeaderMenuButton: true,
-        sortable: true
-      },
-      {
-        field: 'applyForAll',
-        cellStyle: { 'justify-content': "center" },
-        headerName: this.languageS.trans('applyForAll'),
-        width: 100,
-        cellRenderer: ApplyCheckBoxBTNRenderer
-      }
-      ];
-      this.authService.log(this.rowData);
-      for (let key of Object.getOwnPropertyNames(val[0])) {
-        let col = {};
-        if (key != "name") {
-          col['cellStyle'] = { 'justify-content': "center" };
-          col['field'] = key;
-          col['minWidth'] = 100;
-          col['sortable'] = false;
-          col['suppressHeaderMenuButton'] = true;
-          col['headerName'] = this.languageS.trans(key);
-          col['cellRenderer'] = CheckBoxBTNRenderer;
-          this.columnDefs.push(col);
-        }
-      }
-      this.authService.log(this.columnDefs);
     });
-    this.securityService.proxyChanged['basic'] =false;
-    this.securityService.proxyChanged['good'] =false;
-    this.securityService.proxyChanged['bad'] =false;
-    this.securityService.proxyChanged['cephalix'] =false;
+    this.securityService.proxyChanged['basic'] = false;
+    this.securityService.proxyChanged['good'] = false;
+    this.securityService.proxyChanged['bad'] = false;
+    this.securityService.proxyChanged['cephalix'] = false;
   }
 
-  onQuickFilterChanged(){
-    this.proxyApi.setGridOption('quickFilterText', (<HTMLInputElement>document.getElementById('proxyQuickFilter')).value);
+  onQuickFilterChanged() {
+    let filter = (<HTMLInputElement>document.getElementById('proxyQuickFilter')).value.toLowerCase;
   }
   segmentChanged(event) {
-    this.segment   = event.detail.value;
+    this.segment = event.detail.value;
     this.newDomain = "";
   }
 
@@ -101,24 +69,31 @@ export class ProxyComponent implements OnInit {
   }
   readLists(listName): Promise<any[]> {
     return new Promise((resolve, reject) => {
-    let sub1 = this.securityService.getProxyCustom(listName).subscribe(
-      (val) => { resolve(val) },
-      (err) => { this.authService.log(err) },
-      () => { sub1.unsubscribe() }
-    )});
+      let sub1 = this.securityService.getProxyCustom(listName).subscribe(
+        (val) => { resolve(val) },
+        (err) => { this.authService.log(err) },
+        () => { sub1.unsubscribe() }
+      )
+    });
   }
 
-  proxyGridReady(params) {
-    this.proxyApi = params.api;
-    this.authService.log("proxyGridReady");
-  }
-  
   setChanged(changed: boolean) {
     this.securityService.proxyChanged[this.segment] = changed
   }
 
-  redirectToEdit( positiveList) {
-    //TODO
+  toggleAcl(acl, i) {
+    console.log(acl)
+    console.log(this.rowData[i])
+    this.setChanged(true)
+    let tmp = acl[this.groups[0]]
+    for (let group of this.groups) {
+      acl[group] = !tmp
+    }
+    console.log(this.rowData[i])
+  }
+  toggle(acl, group) {
+    this.setChanged(true)
+    acl[group] = !acl[group]
   }
   writeConfig() {
     let list: string[] = [];
@@ -127,10 +102,10 @@ export class ProxyComponent implements OnInit {
         this.authService.log(this.rowData);
         this.objectService.requestSent();
         let sub = this.securityService.setProxyBasic(this.rowData).subscribe(
-          (val) => { 
+          (val) => {
             this.objectService.responseMessage(val)
             this.securityService.proxyChanged[this.segment] = false;
-           },
+          },
           (err) => {
             this.objectService.errorMessage(this.languageS.trans("An error was accoured"));
           },
@@ -140,13 +115,13 @@ export class ProxyComponent implements OnInit {
       case 'positive': { return; }
       default: {
         this.objectService.requestSent();
-        let sub = this.securityService.setProxyCustom(this.segment,this.lists[this.segment]).subscribe(
-          (val) => { 
+        let sub = this.securityService.setProxyCustom(this.segment, this.lists[this.segment]).subscribe(
+          (val) => {
             this.objectService.responseMessage(val)
             this.securityService.proxyChanged[this.segment] = false;
           },
           (err) => { this.objectService.errorMessage(this.languageS.trans("An error was accoured")); },
-          () => {sub.unsubscribe()}
+          () => { sub.unsubscribe() }
         )
       }
     }
@@ -154,7 +129,6 @@ export class ProxyComponent implements OnInit {
   restartProxy() {
     //TODO
   }
-
   addNewDomain() {
     this.lists[this.segment].push(this.newDomain)
     this.lists[this.segment].sort()
@@ -162,8 +136,8 @@ export class ProxyComponent implements OnInit {
     this.newDomain = "";
   }
 
-  deleteDomain(index){
-    this.lists[this.segment].splice(index,1)
+  deleteDomain(index) {
+    this.lists[this.segment].splice(index, 1)
     this.securityService.proxyChanged[this.segment] = true;
   }
 }
